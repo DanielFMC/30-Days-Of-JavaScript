465e13bada5271c0805d863d3c49a80fb16d8c35
Daniel Moreno <daniel970508@gmail.com>
2021-04-06 16:30:27 -0500

    Day 01 complete

---
+++ solutions/day-01/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof stringVariable);
+console.log(typeof booelanVariable);
+console.log(typeof undefinedVariable);
+console.log(typeof title);
---
+++ solutions/day-01/variable.js
@@ -0,0 +1,32 @@
+let stringVariable = "Mi nombre es Daniel!";
+let booelanVariable = true;
+let undefinedVariable;
+let title = null;
+
+let varWithoutValue1;
+let varWithoutValue2;
+let varWithoutValue3;
+let varWithoutValue4;
+
+let varWithValue1 = "Value 1";
+let varWithValue2 = 2;
+let varWithValue3 = `Var with value 3`;
+let varWithValue4 = 3.14;
+
+let myFirstName = "Daniel";
+let myLastName = "Moreno";
+let myMaritalStatus = "Single";
+let myCountry = "Colombia";
+let isMyAge = 23;
+
+let firstName = "Fernando",
+  lastName = "Cáceres",
+  maritalStatus = "Married",
+  country = "Argentina",
+  age = 30;
+
+let myAge = 25;
+let yourAge = 30;
+
+console.log("I am", myAge, "years old");
+console.log("You are", yourAge, "years old");
